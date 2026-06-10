83087c175cb0c7787e6678520afa8e5570d821bf
Raihan Sharif <raihansharif92@gmail.com>
2026-06-10 18:03:40 +0100

    basic styling

--- script.mjs
+++ script.mjs
@@ -1,9 +1,3 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
 import { getUserIds } from "./common.mjs";
 import { handleFormSubmit, handleSelectUser } from "./evenHandlers.mjs";
 import { addData, clearData } from "./storage.mjs";
@@ -28,11 +22,6 @@ function setup() {
 
     form.addEventListener("submit", handleFormSubmit);
 
-    // addData("1", [
-    //     { topic: "Learn JavaScript", date: "2026-07-26" },
-    //     { topic: "Learn JavaScript", date: "2026-08-19" },
-    //     { topic: "Learn JavaScript", date: "2026-10-19" },
-    // ]);
     // clearData("1");
     // clearData("2");
     // clearData("3");
